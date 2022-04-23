b49cd04171f648f0027363678a0e5805019e1f93
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-04-23 19:35:22 +0000

    complete key value pairs for game object

--- scripts/game.js
+++ scripts/game.js
@@ -2,8 +2,8 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: [],
-    
+    choices: ["button1, button2, button3, button4"],
+
 }
 
 module.exports = { game };
